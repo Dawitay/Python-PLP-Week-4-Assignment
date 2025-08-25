5b0185ab8182e02f2bcc62d7303b71882be22649
Dawit Ayele <dawitayelewoldesenbet@gmail.com>
2025-08-25 10:36:53 +0300

    Update Error Handling.py

--- Error Handling.py
+++ Error Handling.py
@@ -1,1 +1,12 @@
 
+# Error Handling Lab 🧪
+
+filename = input("Enter the filename to read: ")
+
+# Open file in read mode
+file = open(filename, "r")
+content = file.read()
+file.close()
+
+print("📄 File content:")
+print(content)
